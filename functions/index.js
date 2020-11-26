c68c62c876382e56dc40da028671e6d7eff3d702
Erick Medeiros <erickmedeiros@ufrn.edu.br>
2020-11-26 17:32:51 -0300

    api CRUD firebase pré-concluída

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,18 @@
 const functions = require('firebase-functions');
+const app = require('express')();
+
+const {
+    getAllTodos,
+    postOneTodo,
+    deleteTodo,
+    editTodo
+} = require('./APIs/todos')
+
+app.get('/todos', getAllTodos);
+app.post('/todos', postOneTodo);
+app.delete('/todos/:todoId', deleteTodo);
+app.put('/todos/:todoId', editTodo);
 
 
+exports.api = functions.https.onRequest(app);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-
- exports.helloWorld = functions.https.onRequest((request, response) => {
-   functions.logger.info("Hello logs!", {structuredData: true});
-   response.send("Hello from Firebase!");
- });
