b2745e697b5256a8fafabe08a7d43ff5e837f6ad
Erick Medeiros <erickmedeiros@ufrn.edu.br>
2020-11-25 23:24:20 -0300

    update sidebar

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './styles.css';
+
+function Header() {
+  return <div className="Header">
+      <h1>Sua lista de compras</h1>
+  </div>;
+}
+
+export default Header;
--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Header from "../Header"
 
 import './styles.css';
 
 const TodoList = () =>
   <div className="TodoList">
-    <h1>Sua lista de compras</h1>
     <form>
+      <Header />
       <input type="checkbox" name="name" />
       <input type="text" />
       <button className="delete">X</button>
