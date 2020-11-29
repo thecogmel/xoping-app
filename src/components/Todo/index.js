217cfb60a05435809425cb896662d6b97e0d3988
Erick Medeiros <erickmedeiros@ufrn.edu.br>
2020-11-29 16:37:02 -0300

    add fucntions add e delete changes db

--- src/components/Todo/index.js
+++ src/components/Todo/index.js
@@ -1,35 +1,60 @@
 import React, { useEffect, useState } from "react";
 import DragIndicatorIcon from '@material-ui/icons/DragIndicator';
+import Checkbox from '@material-ui/core/Checkbox';
+
 import firebase from 'firebase'
 
 import './styles.css';
 
 function Todo() {
 
-  const [item, setItem] = useState('');
+  const [lista, setLista] = useState([{ id: (Math.floor(Math.random() * 100)), value: '', isChecked: false }]);
 
-  function handleChange(e) {
-    firebase.database().ref('/').set(item)
+  function handleChange(list) {
+    firebase.database().ref('/').set(list);
   }
 
-  useEffect(handleChange, [item])
-
-  function handleDelete(ed) {
-    firebase.database().ref(ed).remove(item)
+  function handleDelete(id) {
+    // eslint-disable-next-line
+    const newLista = lista.filter(number => {
+      if (number.id !== id) {
+        return number;
+      }
+    });
+    setLista(newLista);
+    handleChange(newLista);
   }
 
+  function editListItem(value, id) {
+    const newLista = lista.map(number => {
+      if (number.id === id) {
+        return ({...number, value})
+      }
+
+      return number;
+    });
+    setLista(newLista);
+    handleChange(newLista);
+  }
+
+  function toggleChecked(id) {
+    const newLista = lista.map(number => {
+      if (number.id === id) {
+        return ({...number, isChecked:!number.isChecked})
+      }
+
+      return number;
+    });
+    setLista(newLista);
+    handleChange(newLista);
+  }
+  // eslint-disable-next-line
   useEffect(handleDelete, [])
 
-  const [key, setKey] = useState(0);
-  const [lista, setLista] = useState([]);
-
   function addLista() {
-    setKey(key+1);
     setLista([
       ...lista,
-      {
-        id:key
-      }
+      { id: (Math.floor(Math.random() * 100)), value: '', isChecked: false }
     ])
   }
 
@@ -38,27 +63,17 @@ function Todo() {
       <div className="Header">
         <h1>Sua lista de compras</h1>
       </div>
-      <div className="cesta" key='initial'>
-            <DragIndicatorIcon size='29px' />
-            <input type='checkbox'></input>
-            <input type="text"
-              placeholder="Digite um novo item aqui..."
-              value={item}
-              onChange={e => setItem(e.target.value)}
-            />
-            <button onClick={ed => setItem(ed.target.value)}>X</button>
-          </div>
       {lista.map(number => {
         return (
           <div className="cesta" key={number.id}>
-            <DragIndicatorIcon size='29px' />
-            <input type='checkbox'></input>
+            <DragIndicatorIcon />
+            <Checkbox checked={number.isChecked} onChange={() => toggleChecked(number.id)}/>
             <input type="text"
               placeholder="Digite o nome do novo item..."
-              value={item}
-              onChange={e => setItem(e.target.value)}
+              value={number.value}
+              onChange={e => editListItem(e.target.value,number.id)}
             />
-            <button onClick={ed => setItem(ed.target.value)}>X</button>
+            <button onClick={ed => handleDelete(number.id)}>X</button>
           </div>
         );
       })}
