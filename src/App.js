a05769c89a40a73a2d7c6a1ecdf255719557c03b
Erick Medeiros <erickmedeiros@ufrn.edu.br>
2020-11-28 22:44:26 -0300

    adicionar campos implementado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import firebase from 'firebase'
 import Sidebar from './components/Sidebar'
 import Todo from './components/Todo';
 
 import './global.css'
 
 class App extends Component {
+
+  componentWillMount() {
+    var firebaseConfig = {
+        apiKey: "AIzaSyCiypkteEAf5MGxaTHqh6M5AC2ZtRJx2TI",
+        authDomain: "xoping-app.firebaseapp.com",
+        databaseURL: "https://xoping-app.firebaseio.com",
+        projectId: "xoping-app",
+        storageBucket: "xoping-app.appspot.com",
+        messagingSenderId: "372230552974",
+        appId: "1:372230552974:web:4b003f7edbfc36352938df",
+        measurementId: "G-18QSFFHXLR"
+    };
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+  }
   render() {
     return (
       <div id="App">
--- src/components/Todo/index.js
+++ src/components/Todo/index.js
@@ -1,27 +1,72 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import DragIndicatorIcon from '@material-ui/icons/DragIndicator';
-import CloseIcon from '@material-ui/icons/Close';
-import Banco from '../Banco'
-
+import firebase from 'firebase'
 
 import './styles.css';
 
-const Todo = () =>
+function Todo() {
 
-  <div className="Todo">
-    <div className="Header">
-      <h1>Sua lista de compras</h1>
-    </div>
-    <div className="cesta">
-      <DragIndicatorIcon size='29px'/>
-      <input type='checkbox'></input>
-      <input type='text'></input>
-      <CloseIcon size='29px'/>
-    </div>
-    <div>
-      <Banco />
-    </div>
+  const [item, setItem] = useState('');
 
-  </div>;
+  function handleChange(e) {
+    firebase.database().ref('/').set(item)
+  }
 
+  useEffect(handleChange, [item])
+
+  function handleDelete(ed) {
+    firebase.database().ref(ed).remove(item)
+  }
+
+  useEffect(handleDelete, [])
+
+  const [key, setKey] = useState(0);
+  const [lista, setLista] = useState([]);
+
+  function addLista() {
+    setKey(key+1);
+    setLista([
+      ...lista,
+      {
+        id:key
+      }
+    ])
+  }
+
+  return (
+    <div className="Todo">
+      <div className="Header">
+        <h1>Sua lista de compras</h1>
+      </div>
+      <div className="cesta" key='initial'>
+            <DragIndicatorIcon size='29px' />
+            <input type='checkbox'></input>
+            <input type="text"
+              placeholder="Digite um novo item aqui..."
+              value={item}
+              onChange={e => setItem(e.target.value)}
+            />
+            <button onClick={ed => setItem(ed.target.value)}>X</button>
+          </div>
+      {lista.map(number => {
+        return (
+          <div className="cesta" key={number.id}>
+            <DragIndicatorIcon size='29px' />
+            <input type='checkbox'></input>
+            <input type="text"
+              placeholder="Digite o nome do novo item..."
+              value={item}
+              onChange={e => setItem(e.target.value)}
+            />
+            <button onClick={ed => setItem(ed.target.value)}>X</button>
+          </div>
+        );
+      })}
+      <div className="Add">
+        <button onClick={addLista}>+</button>
+        Adicione um novo item...
+      </div>
+    </div>
+  );
+}
 export default Todo;
